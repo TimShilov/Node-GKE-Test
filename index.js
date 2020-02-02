9e5f05c7d2320c8f92b012e4bc65e83f145c15b1
Tim Shilov <tim@shilov.dev>
2020-02-02 20:25:01 +0300

    Persistent storage test

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const PORT = 3000;
 const app = express();
 app.use(morgan('tiny'));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   const lines = [
     'Hello Kubernetes! :-]',
     `Hostname: ${process.env.HOSTNAME}`,
   ];
 
+  fs.writeFileSync(path.join('./', 'storage', `${new Date().toISOString().split('T')}-${process.env.HOSTNAME}`), '');
 
-  fs.readdirSync(__dirname).forEach((file) => {
+  fs.readdirSync(path.join(__dirname, 'storage')).forEach((file) => {
     lines.push(file);
   });
   res.send(
